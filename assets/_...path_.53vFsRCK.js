9eae2a48d231939e16f6dec6a4f1bcbba1a2946e
subB1ss <subB1ss@users.noreply.github.com>
2025-01-22 08:21:16 +0000

    deploy: fb1bbae8e24ebdd9692f2ae1eb88eed9dc61cb8c

---
+++ assets/_...path_.53vFsRCK.js
@@ -0,0 +1,1 @@
+import{u as a,a as s,e as t}from"./chunks/vue-router.CH7l4kx7.js";import{e as n,H as r,P as c,N as p}from"./framework.BVtu4MtS.js";const u=n({__name:"[...path]",setup(f){const o=a(),e=s();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
